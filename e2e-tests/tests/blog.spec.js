35831859c604c606e1013b482b87e646df5786bf
Lassi Oinas <lassi.oinas@hotmail.com>
2025-07-03 19:22:21 +0300

    5.22: blog list e2e testing step6

--- e2e-tests/tests/blog.spec.js
+++ e2e-tests/tests/blog.spec.js
@@ -1,23 +1,30 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
-const { loginWith, createBlog, deleteCreatedBlog } = require('./helpers')
+const { loginWith, logoutAndLogInWith, createBlog, deleteCreatedBlog } = require('./helpers')
 
-const testUser =
+const testUser1 =
   {
     name: 'E2E Tester',
     username: 'e2e-test-user',
     password: 'e2e_secret_password'
   }
 
+const testUser2 =
+  {
+    name: 'Blog Visitor',
+    username: 'guest-1',
+    password: 'guestpw123'
+  }
+
 const testBlog = {
   title: 'Playwright E2E test blog',
-  autor: 'Playwrigth engine',
+  author: 'Playwrigth engine',
   url: 'playwright.dev'
 }
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
     await request.post('http://localhost:5173/api/testing/reset')
-    await request.post('http://localhost:5173/api/users', { data: testUser })
+    await request.post('http://localhost:5173/api/users', { data: testUser1 })
     await page.goto('http://localhost:5173')
   })
 
@@ -31,9 +38,9 @@ describe('Blog app', () => {
 
   describe('Login...', () => {
     test('Succeeds with correct credentials', async ({ page }) => {
-      await loginWith(page, testUser.username, testUser.password)
+      await loginWith(page, testUser1.username, testUser1.password)
       
-      await expect(page.getByText(`${testUser.name} is logged in.`)).toBeVisible()
+      await expect(page.getByText(`${testUser1.name} is logged in.`)).toBeVisible()
     })
 
     test('Fails with wrong credentials', async ({ page }) => {
@@ -46,19 +53,19 @@ describe('Blog app', () => {
 
   describe('When logged in...', () => {
     beforeEach(async ({ page }) => {
-      await loginWith(page, testUser.username, testUser.password)
+      await loginWith(page, testUser1.username, testUser1.password)
       await page.getByText('Show blog creation form').click()
     })
 
     test('A new blog can be created', async ({ page }) => {
-      await createBlog(page, testBlog.title, testBlog.autor, testBlog.url)
+      await createBlog(page, testBlog.title, testBlog.author, testBlog.url)
 
-      await expect(page.locator('.blog-overview')).toBeVisible({ timeout: 10000 })
-      await expect(page.getByRole('button', { name: 'Show details' })).toBeVisible({ timeout: 10000 })
+      await expect(page.locator('.blog-overview')).toBeVisible()
+      await expect(page.getByRole('button', { name: 'Show details' })).toBeVisible()
     })
 
     test('A blog can be liked', async ({ page }) => {
-      await createBlog(page, testBlog.title, testBlog.autor, testBlog.url)
+      await createBlog(page, testBlog.title, testBlog.author, testBlog.url)
       await page.getByRole('button', { name: 'Show details' }).click()
       await page.getByRole('button', { name: 'Like' }).click()
 
@@ -67,11 +74,20 @@ describe('Blog app', () => {
     })
 
     test('A blog can be deleted', async ({ page }) => {
-      await createBlog(page, testBlog.title, testBlog.autor, testBlog.url)
+      await createBlog(page, testBlog.title, testBlog.author, testBlog.url)
       await deleteCreatedBlog(page)
 
       await expect(page.locator('.blog-overview')).not.toBeVisible()
       await expect(page.locator('.blog-details')).not.toBeVisible()
+    })
+
+    test('Only the user who created a blog can see the \'Remove\'-button', async ({ page, request }) => {
+      await request.post('http://localhost:5173/api/users', { data: testUser2 })
+      await createBlog(page, testBlog.title, testBlog.author, testBlog.url)
+      await logoutAndLogInWith(page, testUser2.username, testUser2.password)
+      await page.getByRole('button', { name: 'Show details' }).click()
+
+      await expect(page.getByRole('button', { name: 'Remove' })).not.toBeVisible()
     })
   })
 
--- e2e-tests/tests/helpers.js
+++ e2e-tests/tests/helpers.js
@@ -1,23 +1,25 @@
-const loginWith = async (page, username, password)  => {
+const loginWith = async (page, username, password) => {
   await page.locator('input[name="Username"]').fill(username)
   await page.locator('input[name="Password"]').fill(password)
   await page.getByRole('button', { name: 'Login' }).click()
-  await page.waitForResponse(resp => resp.url().includes('/api/login'))
+}
+
+const logoutAndLogInWith = async (page, username, password) => {
+  await page.getByRole('button', { name: 'Logout' }).click()
+  await loginWith(page, username, password)
 }
 
 const createBlog = async (page, title, author, url) => {
-  await page.locator('.title-input').fill(title)
-  await page.locator('.author-input').fill(author)
-  await page.locator('.url-input').fill(url)
-  await page.locator('.create-button').click()
-  await page.waitForResponse(resp => resp.url().includes('/api/blogs'))
+  await page.locator('input[name="title"]').fill(title)
+  await page.locator('input[name="author"]').fill(author)
+  await page.locator('input[name="url"]').fill(url)
+  await page.getByRole('button', { name: 'Create' }).click()
 }
 
 const deleteCreatedBlog = async (page) => {
   page.on('dialog', dialog => dialog.accept())
   await page.getByRole('button', { name: 'Show details' }).click()
   await page.getByRole('button', { name: 'Remove' }).click()
-  await page.waitForResponse(resp => resp.url().includes('/api/blogs'))
 }
 
-export { loginWith, createBlog, deleteCreatedBlog }
+export { loginWith, logoutAndLogInWith, createBlog, deleteCreatedBlog }
